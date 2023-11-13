1fafa219ff1203813ccba2bef1bd2e1cac309a42
JGM <cnxw4570123@gmail.com>
2023-11-14 02:45:06 +0900

    feat(PreviewService) : add feature retrieving Order
    PreviewService class
    + Order를 받아오는 parseInputToOrder 메소드 추가

--- src/main/java/christmas/service/PreviewService.java
+++ src/main/java/christmas/service/PreviewService.java
@@ -1,11 +1,17 @@
 package christmas.service;
 
 import christmas.constant.Info;
+import christmas.domain.Order;
 import java.time.LocalDate;
+import java.util.stream.Collectors;
 
 public class PreviewService {
 
-    public LocalDate parseInputToVisitDate(int visitDay){
+    public LocalDate parseInputToVisitDate(int visitDay) {
         return LocalDate.of(Info.THIS_YEAR, Info.THIS_MONTH, visitDay);
+    }
+
+    public Order parseInputToOrder(String input) {
+        return Order.fromDetails(input);
     }
 }
--- src/main/java/christmas/validator/Validator.java
+++ src/main/java/christmas/validator/Validator.java
@@ -16,7 +16,6 @@ public class Validator {
                 T input = inputSupplier.get();
                 return inputMapper.apply(input);
             } catch (NumberFormatException | DateTimeException e) {
-                // order일 경우도 메뉴 개수가 숫자임
                 errorHandler.accept(Info.ERROR_MSG_INVALID_DATE);
             } catch (IllegalArgumentException e) {
                 errorHandler.accept(Info.ERROR_MSG_INVALID_ORDER);
