b513c29966084451debf063f0f8cb17951862144
JGM <cnxw4570123@gmail.com>
2023-11-14 20:36:48 +0900

    test(PreviewService) : add test logic for getAllApplicableEvents
    getAllApplicableEvents 메서드
    + 할인이 적용안될 경우 빈 리스트 반환하는지 체크
    + 할인이 들어갈 경우 어떤 할인이 들어갔는지 체크

--- src/test/java/christmas/OwnTest.java
+++ src/test/java/christmas/OwnTest.java
@@ -2,10 +2,14 @@ package christmas;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 import christmas.constant.Info;
 import christmas.controller.PreviewController;
 import christmas.domain.Badge;
+import christmas.domain.Event;
 import christmas.domain.Order;
 import christmas.service.PreviewService;
 import christmas.validator.Validator;
@@ -15,6 +19,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -23,11 +29,13 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class OwnTest {
@@ -54,7 +62,7 @@ public class OwnTest {
     @ParameterizedTest(name = "{index}회차 금액 = {0} 배지 = {1}")
     @CsvSource(value = {"50_000 : SANTA", "19_999 : TREE", "100 : NONE"}, delimiter = ':')
     void 금액에_알맞는_배지를_줍니다(int benefit, Badge badge) {
-        Assertions.assertEquals(badge, Badge.checkBadge(benefit));
+        assertEquals(badge, Badge.checkBadge(benefit));
     }
 
     @ParameterizedTest
@@ -63,11 +71,11 @@ public class OwnTest {
         Supplier<Integer> inputDay = () -> test;
         Function<Integer, LocalDate> inputToVisitDate = previewService::parseInputToVisitDate;
 
-        Assertions.assertTimeoutPreemptively(
+        assertTimeoutPreemptively(
                 Duration.ofSeconds(1L),
                 () -> {
                     LocalDate date = Validator.validate(inputDay, inputToVisitDate, outputView::printErrorMsg);
-                    Assertions.assertEquals(date.getDayOfMonth(), test);
+                    assertEquals(date.getDayOfMonth(), test);
                 }
         );
     }
@@ -91,7 +99,7 @@ public class OwnTest {
             // 1초가 지난 후 byteArrayOutputStream.toString()이 Info.INVALID_DATE를 포함하는지 확인합니다.
             assertThat(byteArrayOutputStream.toString())
                     .contains(Info.ERROR_MSG_INVALID_DATE);
-        }finally {
+        } finally {
             executor.shutdown();
         }
 
@@ -116,15 +124,15 @@ public class OwnTest {
             "아이스크림-1:5000",
             "티본스테이크-1,양송이수프-1:61000",
             "제로콜라-1,레드와인-1,바비큐립-1:117000"
-            }, delimiter = ':')
-    void 올바른_주문의_총_가격을_계산합니다(String orderDetail, int totalPrice){
+    }, delimiter = ':')
+    void 올바른_주문의_총_가격을_계산합니다(String orderDetail, int totalPrice) {
         Order order = Order.fromDetails(orderDetail);
-        Assertions.assertEquals(totalPrice,order.calculateTotalPrice());
+        assertEquals(totalPrice, order.calculateTotalPrice());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"제로콜라-1", "양송이수프-21", "크리스마스파스타-1,크리스마스파스타-1"})
-    void 주문이_올바르지_않습니다(String detail){
+    void 주문이_올바르지_않습니다(String detail) {
         Supplier<String> detailSupplier = () -> detail;
         Function<String, Order> orderMapper = previewService::parseInputToOrder;
 
@@ -135,11 +143,116 @@ public class OwnTest {
 
         try {
             task.get(500L, TimeUnit.MILLISECONDS);
-        }catch (InterruptedException | TimeoutException | ExecutionException e){
+        } catch (InterruptedException | TimeoutException | ExecutionException e) {
             assertThat(byteArrayOutputStream.toString())
                     .contains(Info.ERROR_MSG_INVALID_ORDER);
-        }finally {
+        } finally {
             excutor.shutdown();
         }
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "29:타파스-2,제로콜라-1,초코케이크-1", // 주말에 메인 없이
+            "28:시저샐러드-1,티본스테이크-1,제로콜라-1" // 평일에 디저트 없이
+    }
+            , delimiter = ':')
+    void 적용된_할인이_없습니다(int visitDay, String order) {
+        // given
+        LocalDate visitDate = previewService.parseInputToVisitDate(visitDay);
+        Order userOrder = previewService.parseInputToOrder(order);
+
+        // when
+        List<Event> applicableEvents = previewService.getAllApplicableEvents(userOrder, visitDate);
+
+        // then
+        assertThat(applicableEvents.equals(Collections.EMPTY_LIST));
+    }
+
+
+    private static Stream<Arguments> forDiscountTest() {
+        return Stream.of(
+                Arguments.of(4, "초코케이크-1,제로콜라-1", List.of("평일 할인: -2,023원")),
+                Arguments.of(8, "티본스테이크-1,제로콜라-1", List.of("주말 할인: -2,023원", "크리스마스 디데이 할인: -1,700원")),
+                Arguments.of(29, "티본스테이크-1,바비큐립-1,크리스마스파스타-1,제로콜라-1", List.of("주말 할인: -6,069원", "증정 이벤트: -25,000원")),
+                Arguments.of(31, "초코케이크-10", List.of("평일 할인: -20,230원", "특별 할인: -1,000원", "증정 이벤트: -25,000원")),
+                Arguments.of(25, "초코케이크-10,제로콜라-1", List.of("평일 할인: -20,230원", "크리스마스 디데이 할인: -3,400원","특별 할인: -1,000원", "증정 이벤트: -25,000원"))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("forDiscountTest")
+    void 적용된_이벤트가_일치합니다(int visitDay, String order, List<String> output) {
+        assertAll(
+                () -> {
+                    // given
+                    LocalDate localDate = previewService.parseInputToVisitDate(visitDay);
+                    Order userOrder = previewService.parseInputToOrder(order);
+
+                    // when
+                    List<Event> applicableEvents = previewService.getAllApplicableEvents(userOrder, localDate);
+
+                    // then
+                    assertThat(applicableEvents.stream()
+                            .map(Event::showBenefitDetail)
+                            .toList().equals(output)
+                    );
+                },
+                () -> {
+                    // given
+                    LocalDate localDate = previewService.parseInputToVisitDate(visitDay);
+                    Order userOrder = previewService.parseInputToOrder(order);
+
+                    // when
+                    List<Event> applicableEvents = previewService.getAllApplicableEvents(userOrder, localDate);
+
+                    // then
+                    assertThat(applicableEvents.stream()
+                            .map(Event::showBenefitDetail)
+                            .toList().equals(output));
+                },
+                () -> {
+                    // given
+                    LocalDate localDate = previewService.parseInputToVisitDate(visitDay);
+                    Order userOrder = previewService.parseInputToOrder(order);
+
+                    // when
+                    List<Event> applicableEvents = previewService.getAllApplicableEvents(userOrder, localDate);
+
+                    // then
+                    assertThat(applicableEvents.stream()
+                            .map(Event::showBenefitDetail)
+                            .toList().equals(output)
+                    );
+                },
+                () -> {
+                    // given
+                    LocalDate localDate = previewService.parseInputToVisitDate(visitDay);
+                    Order userOrder = previewService.parseInputToOrder(order);
+
+                    // when
+                    List<Event> applicableEvents = previewService.getAllApplicableEvents(userOrder, localDate);
+
+                    // then
+                    assertThat(applicableEvents.stream()
+                            .map(Event::showBenefitDetail)
+                            .toList().equals(output)
+                    );
+                },
+                () -> {
+                    // given
+                    LocalDate localDate = previewService.parseInputToVisitDate(visitDay);
+                    Order userOrder = previewService.parseInputToOrder(order);
+
+                    // when
+                    List<Event> applicableEvents = previewService.getAllApplicableEvents(userOrder, localDate);
+
+                    // then
+                    assertThat(applicableEvents.stream()
+                            .map(Event::showBenefitDetail)
+                            .toList().equals(output)
+                    );
+                }
+        );
     }
 }
