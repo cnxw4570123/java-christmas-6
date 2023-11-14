51eb601a789e49a74986ecba2d0fd9a1c474549d
JGM <cnxw4570123@gmail.com>
2023-11-14 20:20:47 +0900

    feat(PreviewService) : add feature applying all available events
    PreviewService
    + sumOrderPrice 총 주문 금액을 반환하는 메소드 추가
    + 메뉴를 카테고리별 개수로 바꿔주는 gatherMenuCountByMenuGroup 메소드 추가
    + 총 메뉴 금액을 기반으로 List에 증정품 이벤트를 넣어주는 applyAvailableGiftEvent 메소드 추가
    + 카테고리 별 메뉴 개수를 기반으로 List에 가능한 할인 이벤트를 넣어주는 applyAvailableDiscount 메소드 추가
    + 가능한 할인과 증점품 이벤트 리스트들을 담아 반환하는 getAllAplicableEvents 메소드 추가

--- src/main/java/christmas/service/PreviewService.java
+++ src/main/java/christmas/service/PreviewService.java
@@ -1,9 +1,15 @@
 package christmas.service;
 
 import christmas.constant.Info;
+import christmas.domain.Discount;
+import christmas.domain.Event;
+import christmas.domain.Gift;
+import christmas.domain.MenuGroup;
 import christmas.domain.Order;
 import java.time.LocalDate;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class PreviewService {
 
@@ -14,4 +20,32 @@ public class PreviewService {
     public Order parseInputToOrder(String input) {
         return Order.fromDetails(input);
     }
+
+    public List<Event> getAllApplicableEvents(Order userOrder, LocalDate visitDate){
+        List<Event> applicableEvents = new ArrayList<>();
+        applyAvailableDiscounts(userOrder, visitDate, applicableEvents);
+        applyAvailableGiftEvent(userOrder, applicableEvents);
+        return applicableEvents;
+    }
+
+    private void applyAvailableDiscounts(Order userOrder, LocalDate visitDate, List<Event> applicableEvents) {
+        Map<MenuGroup, Integer> groupByCategory = gatherMenuCountByMenuGroup(userOrder);
+        Discount.applyDayOfWeekDiscount(groupByCategory, visitDate).ifPresent(applicableEvents::add);
+        Discount.applySpecialDiscount(visitDate).ifPresent(applicableEvents::add);
+        Discount.applyChristmasDDayDiscount(visitDate).ifPresent(applicableEvents::add);
+    }
+
+    private void applyAvailableGiftEvent(Order userOrder, List<Event> applicableEvents) {
+        int totalPrice = sumOrderPrice(userOrder);
+        Gift.applyGift(totalPrice).ifPresent(applicableEvents::add);
+    }
+
+    public int sumOrderPrice(Order userOrder){
+        return userOrder.calculateTotalPrice();
+    }
+
+    public Map<MenuGroup, Integer> gatherMenuCountByMenuGroup(Order userOrder){
+        return userOrder.toCountByMenuGroup();
+    }
+
 }
