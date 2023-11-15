941043a9a1a17043ece35b974580f47b13edbb1d
JGM <cnxw4570123@gmail.com>
2023-11-15 20:09:31 +0900

    feat(MenuGroup) : add ETC to MenuGroup
    MenuGroup
    + 카테고리에 해당하지 않는 메뉴는 ETC를 반환하도록 수정함

--- src/main/java/christmas/domain/MenuGroup.java
+++ src/main/java/christmas/domain/MenuGroup.java
@@ -1,14 +1,17 @@
 package christmas.domain;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public enum MenuGroup {
     APPETIZER("에피타이저", List.of(Menu.MUSHROOM_SOUP, Menu.TAPAS, Menu.CAESER_SALAD)),
     MAIN_DISH("메인", List.of(Menu.T_BONE_STEAK, Menu.BARBECUE_RIB, Menu.SEAFOOD_PASTA, Menu.CHRISTMAS_PASTA)),
     DESSERT("디저트", List.of(Menu.CHOCOLATE_CAKE, Menu.ICE_CREAM)),
-    DRINK("음료", List.of(Menu.ZERO_COKE, Menu.RED_WINE, Menu.CHAMPAGNE));
-    private String GroupName;
+    DRINK("음료", List.of(Menu.ZERO_COKE, Menu.RED_WINE, Menu.CHAMPAGNE)),
+
+    ETC("없음", Collections.EMPTY_LIST);
+    private final String GroupName;
     private final List<Menu> menus;
 
     MenuGroup(String groupName, List<Menu> menus) {
@@ -20,6 +23,6 @@ public enum MenuGroup {
         return Arrays.stream(MenuGroup.values())
                 .filter(menuGroup -> menuGroup.menus.contains(menu))
                 .findFirst()
-                .get();
+                .orElse(ETC);
     }
 }
